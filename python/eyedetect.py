cdc8143353997e835ad4cbb4e78c0488facd6d2c
hakmo <hakeem.mohamed@gmail.com>
2015-10-04 12:34:23 -0500

    Update eyedetect.py
    Added comments for later code expansion, taking into account Haar cascade training

--- python/eyedetect.py
+++ python/eyedetect.py
@@ -25,25 +25,32 @@ faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 # check to make sure a face was found, return error if not
 if(len(faces) < 1):
     print 'Error: No face found'
+    # drop image into "no faces found" folder for later analysis
+    # db shoud flag this entry as a problem
     sys.exit(1)
 else:
+    # drop image into "faces found" folder for later Face training
     print len(faces), 'faces found.'
 #box the face
 for (x,y,w,h) in faces:
     cv2.rectangle(input,(x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h, x:x+w]
+    # save off roi_gray into a "found faces" folder for later Eye training
     roi_color = input[y:y+h, x:x+w]
     # find the eyes
     eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 4)
-    # check to make sure 2 eyes were found, return error if not
+    # check to make sure 2 eyes were found, return error if not (note: change to 1 eye, since some people may only have 1)
     if(len(eyes) < 2):
         print 'Error: Not enough eyes found'
+        # drop image into "no eyes found" folder for later analysis
+        # db shoud flag this entry as a problem
         sys.exit(2)
     else:
         print len(eyes), 'eyes found.'
     #box the eyes
     for (ex,ey,ew,eh) in eyes:
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        # save off found eyes into a "found eyes" folder for later training (good eyes/bad eyes/etc)
 
 # display outlined features, wait for key
 cv2.imshow('output',input)
